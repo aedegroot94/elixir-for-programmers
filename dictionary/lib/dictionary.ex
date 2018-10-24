a28dfb05abf412d63a5604ad9fc4aa632ec30053
aedegroot <aedegroot94@gmail.com>
2018-10-24 13:26:44 +0200

    chapter 'our first project' done

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,18 +1,12 @@
 defmodule Dictionary do
-  @moduledoc """
-  Documentation for Dictionary.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Dictionary.hello()
-      :world
-
-  """
   def hello do
-    :world
+    IO.puts "Hello, World!"
+  end
+  def wordlist do
+    content = File.read!("assets/words.txt")
+    String.split(content)
+  end
+  def randomWord do
+    Enum.random(wordlist())
   end
 end
